5ec35fa77b21ea9f491af67903274195f8adffa8
esracanpolat <36516766+esracanpolat@users.noreply.github.com>
2022-11-04 03:33:38 +0300

    edited

--- src/components/Scrool.js
+++ src/components/Scrool.js
@@ -34,18 +34,10 @@ export default function Scrool({ setSelectBrandD }) {
 
     }, []);
     return (
-        <div style={{ overflowX: 'scroll', width: '100vw', display: 'inline-flex', padding: '12px' }}>
-            <div className='d-flex justify-content-space-between align-items-center mt-2 ml-2' style={{ border: '1px solid rgba(236, 238, 239, 1)', overflowX: 'auto', borderRadius: 10, height: 36, width: 'auto' }}>
-                <div>
-                    <img src='/images/searchButton.svg' />
-                </div>
-                <div style={{ textAlign: 'center', marginLeft: 5 }}>
-                    Fırsat Bul
-                </div>
-            </div>
+        <div style={{ overflowX: 'scroll', width: '100vw', display: 'inline-flex', alignItems: 'center' }}>
             {brandData && brandData.map((brand) => (
                 <a style={{ cursor: 'pointer' }} onClick={() => selectBrandHandle(brand.Name)}>,
-                    <div className='d-flex justify-content-space-between align-items-center mt-2 ml-2' style={{ border: selectBrand.includes(brand.Name) === true ? '1px solid blue' : '1px solid rgba(236, 238, 239, 1)', borderRadius: 10, height: 36, width: 105, marginLeft: 8 }}>
+                    <div className='d-flex justify-content-space-between align-items-center ml-2' style={{ border: selectBrand.includes(brand.Name) === true ? '1px solid blue' : '1px solid rgba(236, 238, 239, 1)', borderRadius: 10, height: 36, width: 105, marginLeft: 8 }}>
                         <div>
                             <img style={{ borderRadius: 5, height: 24, width: 24 }} src={brand.IconUrl} />
                         </div>
